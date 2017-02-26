8b9c9d15344282c489e18c21761ba3fbd3908edc
Robert McWilliam <ormiret@finzean.57north.org.uk>
2017-02-26 23:02:28 +0000

    Update admin to use decorators to check logged in and admin, and make admin/behind check for current+last 3 months to match actual definitions for dismemberment and report errors

--- admin.py
+++ admin.py
@@ -15,86 +15,70 @@ def admin_home():
     return render_template('admin.html', members=[m for m in all_member() if m.is_active()])
 
 @app.route('/hub/admin/payment', methods=['POST'])
+@login_required
+@admin_required
 def admin_payment():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)
-        member = Member(request.form['username'])
-        if 'month' in request.form.keys():
-            try:
-                month = int(request.form['month'])
-                if not(1 <= month <= 12):
-                    return render_template('message.html', title='Fail', message='That is an unacceptable month')
-                year = int(request.form['year'])
-                delta_yr = datetime.date.today().year - year
-                if not(-1 <= delta_yr <= 1):
-                    return render_template('message.html', title='Fail', message='That is an unacceptable year')
-                member.add_payment(month, year, session['username'])
-                return redirect('/hub/admin')
-            except Exception as e:
-                return render_template('message.html', title='Fail', message=str(e))
-        else:
-            member.add_payment(int(time.strftime("%m")), int(time.strftime("%Y")), session['username'])
-        return redirect('/hub/admin')
-    else:
-        return redirect('/hub/login')
+    user = Member(session['username'])
+    if not user.is_admin():
+        abort(403)
+    member = Member(request.form['username'])
+    if 'month' in request.form.keys():
+        try:
+            month = int(request.form['month'])
+            if not(1 <= month <= 12):
+                return render_template('message.html', title='Fail', message='That is an unacceptable month')
+            year = int(request.form['year'])
+            delta_yr = datetime.date.today().year - year
+            if not(-1 <= delta_yr <= 1):
+                return render_template('message.html', title='Fail', message='That is an unacceptable year')
+            member.add_payment(month, year, session['username'])
+            return redirect('/hub/admin')
+        except Exception as e:
+            return render_template('message.html', title='Fail', message=str(e))
 
 @app.route('/hub/admin/bank_payment', methods=['POST'])
+@login_requred
+@admin_required
 def admin_bank_payment():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)
-        member = Member(request.form['username'])
-        member.add_bank_payment(int(time.strftime("%m")), int(time.strftime("%Y")), session['username'])
-        return redirect('/hub/admin')
-    else:
-        return redirect('/hub/login')
+    user = Member(session['username'])
+    member = Member(request.form['username'])
+    member.add_bank_payment(int(time.strftime("%m")), int(time.strftime("%Y")), session['username'])
+    return redirect('/hub/admin')
+
     
     
 @app.route('/hub/admin/profile', methods=['GET', 'POST'])
+@login_required
+@admin_required
 def admin_profile():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)        
-        if request.method == 'GET':
-            return render_template('admin_profile_edit.html', profile=Member(request.args['u']).get_profile())
-        elif request.method == 'POST':
-            updatable = ['realname', 'nickname', 'email', 'twitter', 'irc', 'github', 'address']
-            update = {}
-            for x in request.form:
-                if x in updatable:
-                    update[x] = None if request.form[x] == 'None' else request.form[x]
-            m = Member(request.form['username'])
-            m.update_profile(update)
-            return redirect('/hub/admin')
-    else:
-        return redirect('/hub/login')
+    user = Member(session['username'])
+    if request.method == 'GET':
+        return render_template('admin_profile_edit.html', profile=Member(request.args['u']).get_profile())
+    elif request.method == 'POST':
+        updatable = ['realname', 'nickname', 'email', 'twitter', 'irc', 'github', 'address']
+        update = {}
+        for x in request.form:
+            if x in updatable:
+                update[x] = None if request.form[x] == 'None' else request.form[x]
+        m = Member(request.form['username'])
+        m.update_profile(update)
+        return redirect('/hub/admin')
 
 @app.route('/hub/admin/payment_hist')
+@login_required
+@admin_required
 def payment_hist():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)
-        return render_template('admin_payment_hist.html', user=Member(request.args['u']))
-    else:
-        return redirect('/hub/login')
+    return render_template('admin_payment_hist.html', user=Member(request.args['u']))
+    
 
 @app.route('/hub/admin/payment_dashboard')
+@login_required
+@admin_required
 def payment_dashboard():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)
-        hists = {}
-        for m in active_members():
-            hists[m] = Member(m).payment_history()
-        return render_template('admin_payment_dash.html', hists=hists)
-    else:
-        return redirect('/hub/login')
+    hists = {}
+    for m in active_members():
+        hists[m] = Member(m).payment_history()
+    return render_template('admin_payment_dash.html', hists=hists)
 
 @app.route('/hub/admin/applications', methods=['GET', 'POST'])
 @login_required
@@ -155,60 +139,56 @@ def admin_balance():
 
 
 @app.route('/hub/admin/behind')
+@login_required
+@admin_required
 def admin_behind():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)
-        attrs = [int(time.strftime("%m")), int(time.strftime("%Y")),
-                 int(time.strftime("%m")) - 1, int(time.strftime("%Y")),
-                 int(time.strftime("%m")) - 2, int(time.strftime("%Y"))]
-        #handle months being last year
-        if attrs[2] <= 0:
-            attrs[2] = 12 + attrs[2]
-            attrs[3] = attrs[3] - 1
-        if attrs[4] <= 0:
-            attrs[4] = 12 + attrs[4]
-            attrs[5] = attrs[5] -1
-        attrs = tuple(attrs)
-        db = get_db()
-        cur = db.cursor()
-        cur.execute("SELECT username FROM member WHERE username NOT IN (SELECT username FROM payment WHERE (month == ? AND year == ?) OR (month == ? AND year == ?) OR (month == ? AND year == ?)) AND username NOT IN (SELECT user FROM dismembered)", attrs)
-        users = cur.fetchall()
-        behind = []
-        for user in users:
-            m = Member(user[0])
-            try:
-                year, month = m.get_payments()[0][0:2]
-            except Exception:
-                year, month = None, None
-            behind.append((m.username, m.profile["realname"], m.profile["email"], year, month))
-        return render_template('admin_behind.html', behind=behind)
-    else:
-        return redirect('/hub/login')
+    attrs = [int(time.strftime("%m")), int(time.strftime("%Y")),
+             int(time.strftime("%m")) - 1, int(time.strftime("%Y")),
+             int(time.strftime("%m")) - 2, int(time.strftime("%Y")),
+             int(time.strftime("%m")) - 3, int(time.strftime("%Y"))]
+    #handle months being last year
+    # ugh, should really have this as a function...
+    if attrs[2] <= 0:
+        attrs[2] = 12 + attrs[2]
+        attrs[3] = attrs[3] - 1
+    if attrs[4] <= 0:
+        attrs[4] = 12 + attrs[4]
+        attrs[5] = attrs[5] - 1
+    if attrs[6] <= 0:
+        attrs[6] = 12 + attrs[6]
+        attrs[7] = attrs[7] - 1
+    attrs = tuple(attrs)
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("SELECT username FROM member WHERE username NOT IN (SELECT username FROM payment WHERE (month == ? AND year == ?) OR (month == ? AND year == ?) OR (month == ? AND year == ?)) AND username NOT IN (SELECT user FROM dismembered)", attrs)
+    users = cur.fetchall()
+    behind = []
+    errors = ""
+    for user in users:
+        m = Member(user[0])
+        try:
+            year, month = m.get_payments()[0][0:2]
+        except Exception as e:
+            year, month = None, None
+            errors += str(e)
+        behind.append((m.username, m.profile["realname"], m.profile["email"], year, month))
+    return render_template('admin_behind.html', behind=behind, errors=errors)
+
 
 @app.route('/hub/admin/gaps')
+@login_required
+@admin_required
 def admin_gaps():
-    if 'username' in session:
-        user = Member(session['username'])
-        if not user.is_admin():
-            abort(403)
-        gaps = find_gaps(6)
-        return render_template('admin_gaps.html', gaps=gaps)
-    else:
-        return redirect('/hub/login')
-            
+    gaps = find_gaps(6)
+    return render_template('admin_gaps.html', gaps=gaps)
 
 @app.route('/hub/admin/payment_list')
+@login_required
+@admin_required
 def payment_list():
-    if 'username' in session:
-       user = Member(session['username'])
-       if not user.is_admin():
-           abort(403)
-       db = get_db()
-       cur = db.cursor()
-       cur.execute("SELECT * FROM payment ORDER BY id;")
-       payments = cur.fetchall()
-       return render_template('admin_payments.html', payments=payments)
-    else:
-        return redirect('/hub/login')
+    db = get_db()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM payment ORDER BY id;")
+    payments = cur.fetchall()
+    return render_template('admin_payments.html', payments=payments)
+
